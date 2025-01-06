4290ec892b25ddf2d791dd84d332e8b363cf10a2
mattdlong <mattdlong@gmail.com>
2025-01-06 12:59:57 +0700

    fixed issue with vite dependency

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Add this to help with potential path issues
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 })
